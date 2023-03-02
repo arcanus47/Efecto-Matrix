15d666336b77c2eef8f55aae0e2436e7df93a988
arcanus47 <126821851+arcanus47@users.noreply.github.com>
2023-03-02 17:27:52 -0400

    Add files via upload

---
+++ Matrix.py
@@ -0,0 +1,48 @@
+# -------------------
+# |  EFECTO MATRIX |
+# ------------------
+
+import os
+import time
+import random
+
+os.system('color 0a')
+numeros, tiempo = [0,1], 0
+
+while tiempo < 50:
+    print(random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),random.randrange(1,5)* " ",
+    random.choice(numeros),)
+
+    tiempo = tiempo + 0.1
+    time.sleep(0.1)
